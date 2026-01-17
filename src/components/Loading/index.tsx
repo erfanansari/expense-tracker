8752584e3f610eeedbc6b10edc22ddef206bee42
Erfan Ansari <dev.erfanansari@gmail.com>
2026-01-17 20:29:34 +0330

    chore: update loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -9,8 +9,8 @@ import type { LoadingProps } from './@types';
 const Loading: FC<LoadingProps> = ({ message = 'Loading...' }) => {
   return (
     <div className="rounded-xl border border-[#e5e5e5] bg-white p-8 shadow-sm sm:p-16">
-      <div className="flex flex-col items-center gap-2">
-        <Loader2 className="h-6 w-6 animate-spin text-[#0070f3] sm:h-8 sm:w-8" />
+      <div className="flex w-full items-center justify-center gap-2">
+        <Loader2 className="h-6 w-6 animate-spin text-[#525252]" />
         <p className="text-center text-sm font-medium text-[#525252] sm:text-base">{message}</p>
       </div>
     </div>
