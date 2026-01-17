b807854441f5e2b6932fb32dd4277d9eab60eb04
Erfan Ansari <dev.erfanansari@gmail.com>
2026-01-17 22:19:36 +0330

    feat: add danger/error variant for button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -18,7 +18,7 @@ const variantStyles = {
   primary: 'bg-[#000000] hover:bg-[#171717] text-white',
   outline: 'bg-white hover:bg-[#f5f5f5] border border-[#e5e5e5] text-[#525252] hover:text-[#171717]',
   danger:
-    'bg-white hover:bg-[#fef2f2] border border-[#e5e5e5] hover:border-[#ef4444] text-[#525252] hover:text-[#ef4444]',
+    'bg-white hover:bg-[#ea001d]/10 border border-[#e5e5e5] hover:border-[#ea001d] text-[#525252] hover:text-[#ea001d]',
 };
 // <button className="px-4 py-2.5 border border-[#e5e5e5] hover:bg-[#f5f5f5] rounded-lg text-sm font-medium text-[#525252] transition-all"></button>
 
